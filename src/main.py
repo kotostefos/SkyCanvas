3fca96de99b14367777ae0d2b90eaa488747cf35
Stef <stef@stefs-mbp.home>
2026-07-23 00:31:20 +0200

    Initial SkyCanvas project structure

---
+++ src/main.py
@@ -0,0 +1,19 @@
+"""
+SkyCanvas
+First prototype
+"""
+
+from datetime import datetime
+
+
+def main():
+    print("🌌 SkyCanvas started!")
+    print("Preparing the universe...")
+
+    now = datetime.now()
+
+    print(f"Current time: {now}")
+
+
+if __name__ == "__main__":
+    main()
